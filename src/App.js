b546868e4e6453aeb19ceef2de03da07dfbebe45
Tristan <tristanhancock@gmail.com>
2024-02-02 01:19:10 +0530

    updated css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logofill.png';
 import './App.css';
-import { Placeholder } from 'react-bootstrap';
 import React, { useState } from 'react';
 import OpenAI from "openai";
 
@@ -9,62 +8,49 @@ function App() {
 
   const handleTaskSubmit = (event) => {
     event.preventDefault(); // Prevents the default form submit action
-
-    // Handle the task here (e.g., adding it to a list)
     console.log(task); // For demonstration purposes
-
     setTask(''); // Clear the input field after submission
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        {
-                <img src={logo} alt="Logo" className="App-logo" />
-        }
-        
+        <img src={logo} alt="Logo" className="App-logo" />
       </header>
-      <main className="App-body">
-  <div className="task-input-container">
-    <h2 className="task-input-header">
-      Tell Me Everything You Have To Get Done, We Will Help You Pick
-    </h2>
-
-    <form onSubmit={handleTaskSubmit}>
-      <input 
-        type="text" 
-        className="task-input-field" 
-        placeholder="Enter a task..."
-        value={task}
-        onChange={(e) => setTask(e.target.value)}
-      />
-    </form>
-  </div>
-</main>
-
-
-
-
-
       
+      <main className="App-body">
+        <div className="task-input-container">
+          <h2 className="task-input-header">
+            Got too many tasks? Don't know where to start? 
+          </h2>
+          <h3 className="task-input-2"> We can help you</h3>
+        </div>
+      </main>
+
+      <div className="task-bar">
+        <form onSubmit={handleTaskSubmit}>
+          <input 
+            type="text" 
+            className="task-input-field" 
+            placeholder="Enter a task..."
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+          />
+        </form>
+      </div>
 
       <footer className="App-footer">
-        {
-         <a
-         className="TrisContact"
-         href="https://techtris.in/#"
-         target="_blank"
-         rel="noopener noreferrer"
-       >
-         Contact Me-  Tristan Hancock
-       </a>
-        }
-        
+        <a
+          className="TrisContact"
+          href="https://techtris.in/#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Contact Me- Tristan Hancock
+        </a>
       </footer>
- 
-      </div>
-   
-  );
+    </div>
+  )
 }
 
 export default App;
